a79a02b9f94b71d8b71bbc527ef261c114c9670c
S. M. Chase <smchase@protonmail.com>
2018-10-03 14:51:02 -0400

    Finished 54

--- Ass54/main.cpp
+++ Ass54/main.cpp
@@ -1,9 +1,30 @@
 #include <iostream>
+#include <stdio.h>
+#include <string>
 
 using namespace std;
 
+void redFrac (int n, int d)
+{
+    for (int i = 1; i <= n; i++) {
+        if (n % i == 0 && d % i == 0) {
+            n /= i;
+            d /= i;
+        }
+    }
+
+    printf("%i/%i\n", n, d);
+}
+
 int main()
 {
-    cout << "Hello wo rld!" << endl;
-    return 0;
+    int num, den;
+
+    printf("Numerator: ");
+    cin >> num;
+
+    printf("Denomenator: ");
+    cin >> den;
+
+    redFrac(num, den);
 }
