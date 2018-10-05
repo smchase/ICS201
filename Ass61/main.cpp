cc225f44cf9f274e9258e4adaf72f6c5c7e43a34
S. M. Chase <smchase@protonmail.com>
2018-10-05 14:15:19 -0400

    Finished 61 & started 62

--- Ass61/main.cpp
+++ Ass61/main.cpp
@@ -1,9 +1,27 @@
 #include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    int arr[10], big = 0, small = 99;
+    srand(time(NULL));
+
+    for (int i = 0; i < 10; i++)
+        arr[i] = (rand() % 99 + 1);
+
+    for (int i = 0; i < 10; i++) {
+        if (arr[i] > big)
+            big = arr[i];
+
+        if (arr[i] < small)
+            small = arr[i];
+
+        printf("%i\n", arr[i]);
+    }
+
+    printf("\nLargest number: %i\nSmallest number: %i\n", big, small);
 }
