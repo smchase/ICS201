d4232ca7ad09a7b9d38950a4a09cfdb7c3225961
S. M. Chase <smchase@protonmail.com>
2018-10-05 14:27:29 -0400

    Finished 62 & started 63

--- Ass62/main.cpp
+++ Ass62/main.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    srand(time(NULL));
+    int n;
+
+    printf("How many numbers to generate? ");
+    cin >> n;
+    printf("\n");
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        arr[i] = (rand() % 99 + 1);
+
+    for (int i = 0; i < n; i++)
+        printf("%i\n", arr[i]);
 }
