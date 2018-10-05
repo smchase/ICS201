117e5cecdc2de58ce9ccf41364147facc0dc78d1
S. M. Chase <smchase@protonmail.com>
2018-10-05 15:06:55 -0400

    a

--- Ass64/main.cpp
+++ Ass64/main.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <string>
+#include <algorithm>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    int n;
+    srand(time(NULL));
+
+    printf("How many numbers to generate? ");
+    cin >> n;
+
+    int arr[n];
+    for (int i = 0; i < n; i ++) {
+        int r = rand() % 40 + 1;
+        while (arr.count(arr.begin(), arr.end(), r) != 0) {
+            r = rand() % 40 + 1
+        }
+        arr[i] = r;
+    }
 }
