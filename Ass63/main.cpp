caee4b1d180aab89fd845cc180fd6692e6e215a4
S. M. Chase <smchase@protonmail.com>
2018-10-05 14:54:13 -0400

    Finished 63 & started 64

--- Ass63/main.cpp
+++ Ass63/main.cpp
@@ -1,9 +1,35 @@
 #include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <string>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    int arr[10];
+    string odd = "Odds: ", even = "Evens: ";
+    srand(time(NULL));
+
+    for (int i = 0; i < 10; i ++)
+        arr[i] = (rand() % 99 + 1);
+
+    printf("Index | Number\n");
+
+    for (int i = 0; i < 10; i ++)
+        printf("    %i | %i\n", i, arr[i]);
+
+    printf("\n\nOdds: ");
+    for (int i = 0; i < 10; i ++) {
+        if (arr[i] % 2 == 1)
+            printf("%i ", arr[i]);
+    }
+
+    printf("\nEvens: ");
+    for (int i = 0; i < 10; i ++) {
+        if (arr[i] % 2 == 0)
+            printf("%i ", arr[i]);
+    }
+    printf("\n");
 }
