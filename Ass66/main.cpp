7fda4130abd103bf93e17d4f4e6e4256cec793a8
S. M. Chase <smchase@protonmail.com>
2018-10-12 15:04:00 -0400

    66 done FUCK YEAH

--- Ass66/main.cpp
+++ Ass66/main.cpp
@@ -1,91 +1,144 @@
 #include <iostream>
-using namespace std;
-
-int main()
-{
-    int age[] = {23,16,18,12,35,42};
-    string name[] = {"Dave","Mary","Cathy","Peter","John","Kim"};
-    int holdAge=0;
-    string holdName="";
-
-    for(int intC=0;intC<6;intC++){
-        for(int intCtr=0;intCtr<5;intCtr++){
-             if(age[intCtr]>age[intCtr+1]){ //Ascending 1-10, A-Z
-                holdAge=age[intCtr];
-                age[intCtr]=age[intCtr+1];
-                age[intCtr+1]=holdAge;
-
-                holdName=name[intCtr];
-                name[intCtr]=name[intCtr+1];
-                name[intCtr+1]=holdName;
-             }
-        }
-    }
-
-    cout<<"List(Sorted)- According to Age"<<endl;
-    for(int intC=0; intC<6;intC++){
-        cout<<name[intC]<<"\t"<<age[intC]<<endl;
-    }
-return 0;
-}
-
-
-#include <iostream>
-#include <stdlib.h>
 #include <stdio.h>
-#include <time.h>
 #include <string>
-#include <algorithm>
-#include <vector>
-#include <cstring>
-#include <algorithm>
 
 using namespace std;
 
-struct data
-{
-    string names[6];
-    float prices[6];
-    int sold[6];
-    float total[6];
-} ;
-
 int main()
 {
-    srand (time(NULL));
-    string names[6] = {"Tulips", "Orchids", "Mixed Stems", "Baby Roses", "Cactus Plants", "Red Roses"};
-    float prices[6] = {15.50, 27.00, 3.95, 18.50, 7.65, 19.00};
-    int sold[6] = {12, 7, 35, 15, 18, 21};
-    float total[6];
+    string names[6] = {"Tulips", "Orchids", "Mixed Stems", "Baby Roses", "Cactus Plants", "Red Roses"}, holdName, headers[4] = {"Name", "Unit Price", "Quantity Sold", "Total Sales"};
+    float prices[6] = {15.50, 27.00, 3.95, 18.50, 7.65, 19.00}, holdPrice;
+    int sold[6] = {12, 7, 35, 15, 18, 21}, holdSold, place;
+    float total[6], holdTotal, big = 0, small = 10000;
+    char input;
 
     for (int i = 0; i < 6; i ++)
         total[i] = prices[i] * sold[i];
 
-    string headers[4] = {"Name", "Unit Price", "Quantity Sold", "Total Sales"};
-    for (int i = 0; i < 4; i ++) {
-        printf("%-*s", 15, headers[i].c_str());
-    }
+    printf("To select an item from the menu enter its corresponding letter\na: View all of the data sorted by total sales (descending)\nb: View all of the data sorted by name (ascending)\nc: View the name of the highest quantity sold\nd: View the name of the lowest quantity sold\ne: Exit\n\nLetter: ");
+    cin >> input;
+    printf("\n");
 
+    while (input != 'e') {
+        switch (input) {
+            case 'a':
+                for (int i = 0; i < 4; i ++)
+                    printf("%-*s", 15, headers[i].c_str());
 
+                for (int i = 0; i < 5; i ++) {
+                    for (int i = 0; i < 5; i ++) {
+                        if (total[i] < total[i + 1]) {
+                            holdName = names[i];
+                            holdPrice = prices[i];
+                            holdSold = sold[i];
+                            holdTotal = total[i];
 
-    for (int n = 0; n < 6; n ++) {
-        printf("\n");
-        for (int i = 0; i < 4; i ++) {
-            switch (i) {
-                case 0:
-                    printf("%-*s", 15, names[n].c_str());
-                    break;
-                case 1:
-                    printf("$%-*.2f", 14, prices[n]);
-                    break;
-                case 2:
-                    printf("%-*i", 15, sold[n]);
-                    break;
-                case 3:
-                    printf("$%.2f", total[n]);
-                    break;
-            }
+                            names[i] = names[i + 1];
+                            prices[i] = prices[i + 1];
+                            sold[i] = sold[i + 1];
+                            total[i] = total[i + 1];
+
+                            names[i + 1] = holdName;
+                            prices[i + 1] = holdPrice;
+                            sold[i + 1] = holdSold;
+                            total[i + 1] = holdTotal;
+                        }
+                    }
+                }
+
+                for (int n = 0; n < 6; n ++) {
+                    printf("\n");
+                    for (int i = 0; i < 4; i ++) {
+                        switch (i) {
+                            case 0:
+                                printf("%-*s", 15, names[n].c_str());
+                                break;
+                            case 1:
+                                printf("$%-*.2f", 14, prices[n]);
+                                break;
+                            case 2:
+                                printf("%-*i", 15, sold[n]);
+                                break;
+                            case 3:
+                                printf("$%.2f", total[n]);
+                                break;
+                        }
+                    }
+                }
+                break;
+
+            case 'b':
+                for (int i = 0; i < 4; i ++)
+                    printf("%-*s", 15, headers[i].c_str());
+
+                for (int i = 0; i < 5; i ++) {
+                    for (int i = 0; i < 5; i ++) {
+                        if (names[i] > names[i + 1]) {
+                            holdName = names[i];
+                            holdPrice = prices[i];
+                            holdSold = sold[i];
+                            holdTotal = total[i];
+
+                            names[i] = names[i + 1];
+                            prices[i] = prices[i + 1];
+                            sold[i] = sold[i + 1];
+                            total[i] = total[i + 1];
+
+                            names[i + 1] = holdName;
+                            prices[i + 1] = holdPrice;
+                            sold[i + 1] = holdSold;
+                            total[i + 1] = holdTotal;
+                        }
+                    }
+                }
+
+                for (int n = 0; n < 6; n ++) {
+                    printf("\n");
+                    for (int i = 0; i < 4; i ++) {
+                        switch (i) {
+                            case 0:
+                                printf("%-*s", 15, names[n].c_str());
+                                break;
+                            case 1:
+                                printf("$%-*.2f", 14, prices[n]);
+                                break;
+                            case 2:
+                                printf("%-*i", 15, sold[n]);
+                                break;
+                            case 3:
+                                printf("$%.2f", total[n]);
+                                break;
+                        }
+                    }
+                }
+                break;
+
+            case 'c':
+                for (int i = 0; i < 6; i ++) {
+                    if (total[i] > big) {
+                        big = total[i];
+                        place = i;
+                    }
+                }
+
+                printf("%s has the most total sales", names[place].c_str());
+                break;
+
+            case 'd':
+                for (int i = 0; i < 6; i ++) {
+                    if (total[i] < small) {
+                        small = total[i];
+                        place = i;
+                    }
+                }
+
+                printf("%s has the least total sales", names[place].c_str());
+                break;
         }
+
+        printf("\n\nTo select an item from the menu enter its corresponding letter: ");
+        cin >> input;
+        printf("\n");
     }
 }
 
