65fe74fa625754364ecb764becc8274cfbd48cda
S. M. Chase <smchase@protonmail.com>
2018-11-14 14:24:56 -0500

    finished 84

---
+++ Ass84.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int digSum (int n, int s)
+{
+    if (n != 0) {
+        digSum((n-(n%10))/10, s+(n%10));
+    }
+    else return s;
+}
+
+int main ()
+{
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+    cout << digSum(num, 0) << endl;
+}
