5e6045c8e1a2be7ce5df176da36c929cf7497add
S. M. Chase <smchase@protonmail.com>
2018-11-14 14:17:32 -0500

    finished 83

--- Ass83.cpp
+++ Ass83.cpp
@@ -2,14 +2,14 @@
 
 using namespace std;
 
-void tri (int n1, int n2) {
-    if (n1 <= n2) {
-        for (int i = 0; i < n1; i ++) {
-            cout << "*";
-        }
-        cout << endl;
-        tri(n1 + 1, n2);
+void tri (int n1, int n2, bool up, int n) {
+    if (n == n2 && up) up = false;
+    for (int i = 0; i < n; i ++) {
+        cout << "*";
     }
+    cout << endl;
+    if (n == n1 && !up) return;
+    tri(n1, n2, up, up ? n + 1 : n - 1);
 }
 
 int main ()
@@ -19,5 +19,6 @@ int main ()
     cin >> num1;
     cout << "Enter a larger number: ";
     cin >> num2;
-    tri(num1, num2);
+    cout << endl;
+    tri(num1, num2, true, num1);
 }
