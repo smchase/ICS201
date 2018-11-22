a99aa3c32efa9e5f7774a9fe2dbb44c3b8cf6113
S. M. Chase <smchase@protonmail.com>
2018-11-22 15:04:20 -0500

    it runs now. maybe use strings rather than arrays.

--- braille.cpp
+++ braille.cpp
@@ -4,28 +4,161 @@
 #include <vector>
 #include <string>
 #include <stdio.h>
+#include <algorithm>
 
 using namespace std;
 
 int main ()
 {
     fstream f("H:\\Documents\\Assignments\\DATA21.txt");
-    string out;
-    vector<vector<string>> br;
+    string out, ltr[3];
+    vector<string> br;
     vector<string> txt;
-    int n = 0;
+    string ref[26][3] = {
+        { // a
+            "xo",
+            "oo",
+            "oo"
+        },
+        { // b
+            "xo",
+            "xo",
+            "oo"
+        },
+        { // c
+            "xx",
+            "oo",
+            "oo"
+        },
+        { // d
+            "xx",
+            "ox",
+            "oo"
+        },
+        { // e
+            "xo",
+            "ox",
+            "oo"
+        },
+        { // f
+            "xx",
+            "xo",
+            "oo"
+        },
+        { // g
+            "xx",
+            "xx",
+            "oo"
+        },
+        { // h
+            "xo",
+            "xx",
+            "oo"
+        },
+        { // i
+            "ox",
+            "xo",
+            "oo"
+        },
+        { // j
+            "ox",
+            "xx",
+            "oo"
+        },
+        { // k
+            "xo",
+            "oo",
+            "xo"
+        },
+        { // l
+            "xo",
+            "xo",
+            "xo"
+        },
+        { // m
+            "xx",
+            "oo",
+            "xo"
+        },
+        { // n
+            "xx",
+            "ox",
+            "xo"
+        },
+        { // o
+            "xo",
+            "ox",
+            "xo"
+        },
+        { // p
+            "xx",
+            "xo",
+            "xo"
+        },
+        { // q
+            "xx",
+            "xx",
+            "xo"
+        },
+        { // r
+            "xo",
+            "xx",
+            "xo"
+        },
+        { // s
+            "ox",
+            "xo",
+            "xo"
+        },
+        { // t
+            "ox",
+            "xx",
+            "xo"
+        },
+        { // u
+            "xo",
+            "oo",
+            "xx"
+        },
+        { // v
+            "xo",
+            "xo",
+            "xx"
+        },
+        { // w
+            "ox",
+            "xx",
+            "ox"
+        },
+        { // x
+            "xx",
+            "oo",
+            "xx"
+        },
+        { // y
+            "xx",
+            "ox",
+            "xx"
+        },
+        { // z
+            "xo",
+            "ox",
+            "xx"
+        }
+    };
 
     while (!f.eof()) {
         getline(f, out);
-        cout << out << endl;
-        if (n % 3 == 0) br.push_back();
-        br[(int)n/3].push_back(out);
-        n ++;
+        br.push_back(out);
     }
 
-    for (int i = 0; i < br.size(); i ++) {
-        for (int j = 0; j < br[i].size(); j ++) {
-            cout << br[i][j];
+    for (int i = 0; i < br.size(); i += 3) {
+        for (int j = 0; j < br[i].length(); j += 2) {
+            ltr[0] = br[i].substr(j, 2);
+            ltr[1] = br[i].substr(j+1, 2);
+            ltr[2] = br[i].substr(j+2, 2);
+
+            printf("%i\n", distance(ref, find(ref, ref+26, ltr)));
         }
     }
 }
