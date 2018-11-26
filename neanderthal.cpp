5d74f49beac3eb3a939e990eff3854f0b34f0a6b
S. M. Chase <smchase@protonmail.com>
2018-11-26 14:36:25 -0500

    started hard thing :(((((

---
+++ neanderthal.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <stdio.h>
+#include <algorithm>
+
+using namespace std;
+
+int main ()
+{
+    cout << "i wanna die";
+}
