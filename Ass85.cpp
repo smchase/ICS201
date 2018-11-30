59965002c0318441a59145911befd8b772f97342
Simon-Chase <smchase@protonmail.com>
2018-11-30 17:45:18 -0500

    working on maze

--- Ass85.cpp
+++ Ass85.cpp
@@ -5,49 +5,84 @@
 #include <vector>
 #include <stdlib.h>
 #include <time.h>
+#include <algorithm>
 
 using namespace std;
 
-void solve (vector<string> &mz, int m[2], int c[2], int &s, int mvs)
+void solve (string mz[], int m[2], int c[2], int l, vector<string> &p, int mvs, int &s)
 {
-    
+    cout << "test" << endl;
+    if (m[0] == c[0] && m[1] == c[0]) {
+        if (mvs < s) {
+            copy(mz, mz+l, p.begin());
+            s = mvs;
+        }
+    } else {
+        if (mz[m[0]][m[1]] == ' ')
+            mz[m[0]][m[1]] = '+';
+        if (mz[m[0]+1][m[1]] == ' ') {
+            m[0] ++;
+            solve(mz, m, c, l, p, mvs+1, s);
+            m[0] --;
+        }
+        if (mz[m[0]-1][m[1]] == ' ') {
+            m[0] --;
+            solve(mz, m, c, l, p, mvs+1, s);
+            m[0] ++;
+        }
+        if (mz[m[0]][m[1]+1] == ' ') {
+            m[1] ++;
+            solve(mz, m, c, l, p, mvs+1, s);
+            m[1] --;
+        }
+        if (mz[m[0]][m[1]-1] == ' ') {
+            m[1] --;
+            solve(mz, m, c, l, p, mvs+1, s);
+            m[1] ++;
+        }
+    }
 }
 
 int main ()
 {
     // School: H:\\Documents\\Assignments
     // Home: C:\\Users\\dumba\\OneDrive\\Documents\\GitHub\\Assignments
-    string location = "H:\\Documents\\Assignments";
+    string location = "C:\\Users\\dumba\\OneDrive\\Documents\\GitHub\\Assignments";
 
     srand(time(NULL));
-    vector<string> maze;
+    vector<string> mazeV;
     fstream file(location + "\\maze.txt");
     string out;
     int mouse[2], cheese[2];
 
-    while (!file.eof()) {
-        getline(file, out);
-        maze.push_back(out);
-    }
+    while (getline(file, out)) mazeV.push_back(out);
 
     do {
-        mouse[0] = rand()%12;
-        mouse[1] = rand()%12;
-    } while (maze[mouse[0]][mouse[1]] != ' ');
-    maze[mouse[0]][mouse[1]] = 'M';
+        mouse[0] = rand()%mazeV.size();
+        mouse[1] = rand()%mazeV[mouse[0]].length();
+    } while (mazeV[mouse[0]][mouse[1]] != ' ');
+    mazeV[mouse[0]][mouse[1]] = 'M';
 
     do {
         cheese[0] = rand()%12;
         cheese[1] = rand()%12;
-    } while (maze[cheese[0]][cheese[1]] != ' ');
-    maze[cheese[0]][cheese[1]] = 'C';
+    } while (mazeV[cheese[0]][cheese[1]] != ' ');
+    mazeV[cheese[0]][cheese[1]] = 'C';
 
-    for (int i = 0; i < maze.size(); i ++) {
-        printf("%s\n", maze[i].c_str());
+    for (int i = 0; i < mazeV.size(); i ++) {
+        printf("%s\n", mazeV[i].c_str());
     }
 
-    int shortest = 30;
-    solve(maze, mouse, cheese, shortest, 0);
+    string maze[mazeV.size()];
+    copy(mazeV.begin(), mazeV.end(), maze);
+    vector<string> path;
+    int shortest = 100;
+    solve(maze, mouse, cheese, mazeV.size(), path, 0, shortest);
+
+    printf("\n");
+    for (int i = 0; i < path.size(); i ++) {
+        printf("%s\n", path[i].c_str());
+    }
 }
 
 /*
