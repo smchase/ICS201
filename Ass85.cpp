546c68882235f04ff511bf771d7b1cc21525174b
S. M. Chase <smchase@protonmail.com>
2018-11-30 14:54:49 -0500

    began perfecting maze

--- Ass85.cpp
+++ Ass85.cpp
@@ -2,44 +2,15 @@
 #include <fstream>
 #include <string>
 #include <stdio.h>
+#include <vector>
 #include <stdlib.h>
 #include <time.h>
 
 using namespace std;
 
-void solve (char mz[12][12], int m[2], int c[2], char d)
+void solve (vector<string> &mz, int m[2], int c[2], int &s, int mvs)
 {
-    if (m[0] == c[0] && m[1] == c[1]) {
-        printf("\n");
-        for (int i = 0; i < 12; i ++) {
-            for (int j = 0; j < 12; j ++) {
-                printf("%c", c[0]==i&&c[1]==j ? 'C' : (mz[i][j] == 'M' || mz[i][j] == '%' ? mz[i][j] : ' '));
-            }
-            printf("\n");
-        }
-        printf("\n");
-        for (int i = 0; i < 12; i ++) {
-            for (int j = 0; j < 12; j ++) {
-                printf("%c", c[0]==i&&c[1]==j ? 'C' : mz[i][j]);
-            }
-            printf("\n");
-        }
-    } else {
-        if (mz[m[0]][m[1]] != 'M') mz[m[0]][m[1]] = 'o';
-
-        char arr[4] = {'^', 'v', '>', '<'};
-        do {
-            d = arr[rand()%4];
-        } while (d == '>' && mz[m[0]][m[1]+1] == '%' || d == '<' && mz[m[0]][m[1]-1] == '%' || d == '^' && mz[m[0]-1][m[1]] == '%' || d == 'v' && mz[m[0]+1][m[1]] == '%');
-
-        if (d == '>') m[1] ++;
-        else if (d == '<') m[1] --;
-        else if (d == '^') m[0] --;
-        else if (d == 'v') m[0] ++;
-
-        printf("%c", d);
-        solve(mz, m, c, d);
-    }
+    
 }
 
 int main ()
@@ -49,17 +20,14 @@ int main ()
     string location = "H:\\Documents\\Assignments";
 
     srand(time(NULL));
-    char maze[12][12];
+    vector<string> maze;
     fstream file(location + "\\maze.txt");
     string out;
-    int y = 0, mouse[2], cheese[2];
+    int mouse[2], cheese[2];
 
     while (!file.eof()) {
         getline(file, out);
-        for (int x = 0; x < 12; x ++) {
-            maze[y][x] = out[x];
-        }
-        y ++;
+        maze.push_back(out);
     }
 
     do {
@@ -72,8 +40,14 @@ int main ()
         cheese[0] = rand()%12;
         cheese[1] = rand()%12;
     } while (maze[cheese[0]][cheese[1]] != ' ');
+    maze[cheese[0]][cheese[1]] = 'C';
 
-    solve(maze, mouse, cheese, '>');
+    for (int i = 0; i < maze.size(); i ++) {
+        printf("%s\n", maze[i].c_str());
+    }
+
+    int shortest = 30;
+    solve(maze, mouse, cheese, shortest, 0);
 }
 
 /*
