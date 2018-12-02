2d42fc4b311ffd9b9c6756156b75ad5fc5ccfbeb
Simon-Chase <smchase@protonmail.com>
2018-12-02 12:10:51 -0500

    HEY IT WORKS FUCK YEAH

--- Ass85.cpp
+++ Ass85.cpp
@@ -3,43 +3,49 @@
 #include <string>
 #include <stdio.h>
 #include <vector>
-#include <stdlib.h>
-#include <time.h>
-#include <algorithm>
+#include <array>
 
 using namespace std;
 
-void solve (string mz[], int m[2], string *p, int mvs, int &s)
-{
-    if (mz[m[0]][m[1]] == 'C') {
-        if (mvs < s) {
-            copy(mz, mz+12, p);
-            s = mvs;
+struct coor {
+    array<int, 2> pos;
+    vector<string> path;
+};
+
+void solve(vector<coor> &coorList, vector<string> &maze, array<int, 2> &cheese, int i) {
+    if (coorList[i].pos == cheese) {
+        printf("\nShortest Path:\n");
+        for (int j = 0; j < coorList[i].path.size(); j ++) {
+            printf("%i. %s\n", j+1, coorList[i].path[j].c_str());
         }
+        return;
     } else {
-        if (mz[m[0]][m[1]] == ' ')
-            mz[m[0]][m[1]] = '+';
-        if (mz[m[0]+1][m[1]] == ' ' || mz[m[0]+1][m[1]] == 'C') {
-            m[0] ++;
-            solve(mz, m, p, mvs+1, s);
-            m[0] --;
+        if (maze[coorList[i].pos[0]][coorList[i].pos[1]-1] == ' ' || maze[coorList[i].pos[0]][coorList[i].pos[1]-1] == 'C') {
+            maze[coorList[i].pos[0]][coorList[i].pos[1]-1] = '+';
+            coorList[i].path.push_back("UP");
+            coorList.push_back({{coorList[i].pos[0], coorList[i].pos[1]-1}, coorList[i].path});
+            coorList[i].path.pop_back();
         }
-        if (mz[m[0]-1][m[1]] == ' ' || mz[m[0]-1][m[1]] == 'C') {
-            m[0] --;
-            solve(mz, m, p, mvs+1, s);
-            m[0] ++;
+        if (maze[coorList[i].pos[0]+1][coorList[i].pos[1]] == ' ' || maze[coorList[i].pos[0]+1][coorList[i].pos[1]] == 'C') {
+            maze[coorList[i].pos[0]+1][coorList[i].pos[1]] = '+';
+            coorList[i].path.push_back("RIGHT");
+            coorList.push_back({{coorList[i].pos[0]+1, coorList[i].pos[1]}, coorList[i].path});
+            coorList[i].path.pop_back();
         }
-        if (mz[m[0]][m[1]+1] == ' ' || mz[m[0]][m[1]+1] == 'C') {
-            m[1] ++;
-            solve(mz, m, p, mvs+1, s);
-            m[1] --;
+        if (maze[coorList[i].pos[0]][coorList[i].pos[1]+1] == ' ' || maze[coorList[i].pos[0]][coorList[i].pos[1]+1] == 'C') {
+            maze[coorList[i].pos[0]][coorList[i].pos[1]+1] = '+';
+            coorList[i].path.push_back("DOWN");
+            coorList.push_back({{coorList[i].pos[0], coorList[i].pos[1]+1}, coorList[i].path});
+            coorList[i].path.pop_back();
         }
-        if (mz[m[0]][m[1]-1] == ' ' || mz[m[0]][m[1]-1] == 'C') {
-            m[1] --;
-            solve(mz, m, p, mvs+1, s);
-            m[1] ++;
+        if (maze[coorList[i].pos[0]-1][coorList[i].pos[1]] == ' ' || maze[coorList[i].pos[0]-1][coorList[i].pos[1]] == 'C') {
+            maze[coorList[i].pos[0]-1][coorList[i].pos[1]] = '+';
+            coorList[i].path.push_back("LEFT");
+            coorList.push_back({{coorList[i].pos[0]-1, coorList[i].pos[1]}, coorList[i].path});
+            coorList[i].path.pop_back();
         }
     }
+    solve(coorList, maze, cheese, i+1);
 }
 
 int main ()
@@ -47,54 +53,22 @@ int main ()
     // School: H:\\Documents\\Assignments
     // Home: C:\\Users\\dumba\\OneDrive\\Documents\\GitHub\\Assignments
     string location = "C:\\Users\\dumba\\OneDrive\\Documents\\GitHub\\Assignments";
-
-    srand(time(NULL));
-    vector<string> mazeV;
     fstream file(location + "\\maze.txt");
     string out;
-    int mouse[2], cheese[2];
+    vector<string> maze;
+    while (getline(file, out)) maze.push_back(out);
 
-    while (getline(file, out)) mazeV.push_back(out);
-
-    do {
-        mouse[0] = rand()%12;
-        mouse[1] = rand()%12;
-    } while (mazeV[mouse[0]][mouse[1]] != ' ');
-    mazeV[mouse[0]][mouse[1]] = 'M';
-
-    do {
-        cheese[0] = rand()%12;
-        cheese[1] = rand()%12;
-    } while (mazeV[cheese[0]][cheese[1]] != ' ');
-    mazeV[cheese[0]][cheese[1]] = 'C';
-
-    for (int i = 0; i < 12; i ++) {
-        printf("%s\n", mazeV[i].c_str());
+    array<int, 2> mouse, cheese;
+    for (int i = 0; i < maze.size(); i ++) {
+        for (int j = 0; j < maze[i].length(); j ++) {
+            if (maze[i][j] == 'M') mouse = {i, j};
+            if (maze[i][j] == 'C') cheese = {i, j};
+            printf("%c", maze[i][j]);
+        }
+        printf("\n");
     }
 
-    string maze[12];
-    copy(mazeV.begin(), mazeV.end(), maze);
-    string path[12];
-    int shortest = 100;
-    solve(maze, mouse, path, 0, shortest);
-
-    printf("\n");
-    for (int i = 0; i < 12; i ++) {
-        printf("%s\n", path[i].c_str());
-    }
-    printf("Shortest Path: %i\n", shortest);
+    vector<coor> coorList;
+    coorList.push_back({mouse, {}});
+    solve(coorList, maze, cheese, 0);
 }
-
-/*
-1.	A mouse in a maze is searching for cheese (#) located at an unknown position in the maze.  The mouse can move in four directions (up, down, left, right).  The mouse begins at a position in the maze marked by (s) and must search through the maze until it finds the cheese without going through any walls (%).  Output the path the mouse could take to find the cheese.
-%%%%%%%%%%%%%%%%
-%%             %
-%%%   %   %    s
-%%%   %%%%%%%%%%
-%%      %%%%%%%%
-%     %%%      %
-%%%%%      %%  %
-%%%%%%%%%%%%% %%
-%#            %%
-%%%%%%%%%%%%%%%%
-*/
