dda3b0bb7d1c320795f89091a39b5f6c4261cf6f
Simon-Chase <smchase@protonmail.com>
2018-12-01 20:21:25 -0500

    why wont this work

--- Ass85.cpp
+++ Ass85.cpp
@@ -9,37 +9,47 @@
 
 using namespace std;
 
-void solve (string mz[], int m[2], int c[2], int l, vector<string> &p, int mvs, int &s)
+void solve (string mz[], int m[2], int c[2], vector<string> &p, int mvs, int &s)
 {
-    cout << "test" << endl;
     if (m[0] == c[0] && m[1] == c[0]) {
         if (mvs < s) {
-            copy(mz, mz+l, p.begin());
+            copy(mz, mz+12, p.begin());
             s = mvs;
         }
+        cout << "SOLVED" << endl;
     } else {
         if (mz[m[0]][m[1]] == ' ')
             mz[m[0]][m[1]] = '+';
-        if (mz[m[0]+1][m[1]] == ' ') {
+        if (mz[m[0]+1][m[1]] == ' ' || mz[m[0]+1][m[1]] == 'C') {
+            cout << "down" << endl;
             m[0] ++;
-            solve(mz, m, c, l, p, mvs+1, s);
+            solve(mz, m, c, p, mvs+1, s);
             m[0] --;
         }
-        if (mz[m[0]-1][m[1]] == ' ') {
+        if (mz[m[0]-1][m[1]] == ' ' || mz[m[0]-1][m[1]] == 'C') {
+            cout << "up" << endl;
             m[0] --;
-            solve(mz, m, c, l, p, mvs+1, s);
+            solve(mz, m, c, p, mvs+1, s);
             m[0] ++;
         }
-        if (mz[m[0]][m[1]+1] == ' ') {
+        if (mz[m[0]][m[1]+1] == ' ' || mz[m[0]][m[1]+1] == 'C') {
+            cout << "right" << endl;
             m[1] ++;
-            solve(mz, m, c, l, p, mvs+1, s);
+            solve(mz, m, c, p, mvs+1, s);
             m[1] --;
         }
-        if (mz[m[0]][m[1]-1] == ' ') {
+        if (mz[m[0]][m[1]-1] == ' ' || mz[m[0]][m[1]-1] == 'C') {
+            cout << "left" << endl;
             m[1] --;
-            solve(mz, m, c, l, p, mvs+1, s);
+            solve(mz, m, c, p, mvs+1, s);
             m[1] ++;
         }
+        //cout << "DONE";
+        /*cout << mz[m[0]+1][m[1]] << endl;
+        cout << mz[m[0]-1][m[1]] << endl;
+        cout << mz[m[0]][m[1]+1] << endl;
+        cout << mz[m[0]][m[1]-1] << endl;
+        cout << m[0] << m[1];*/
     }
 }
 
@@ -58,18 +68,20 @@ int main ()
     while (getline(file, out)) mazeV.push_back(out);
 
     do {
-        mouse[0] = rand()%mazeV.size();
-        mouse[1] = rand()%mazeV[mouse[0]].length();
+        mouse[0] = rand()%12;
+        mouse[1] = rand()%12;
     } while (mazeV[mouse[0]][mouse[1]] != ' ');
     mazeV[mouse[0]][mouse[1]] = 'M';
 
     do {
-        cheese[0] = rand()%12;
-        cheese[1] = rand()%12;
+        /*cheese[0] = rand()%12;
+        cheese[1] = rand()%12;*/
+        cheese[0] = mouse[0] + 1;
+        cheese[1] = mouse[1];
     } while (mazeV[cheese[0]][cheese[1]] != ' ');
     mazeV[cheese[0]][cheese[1]] = 'C';
 
-    for (int i = 0; i < mazeV.size(); i ++) {
+    for (int i = 0; i < 12; i ++) {
         printf("%s\n", mazeV[i].c_str());
     }
 
@@ -77,10 +89,10 @@ int main ()
     copy(mazeV.begin(), mazeV.end(), maze);
     vector<string> path;
     int shortest = 100;
-    solve(maze, mouse, cheese, mazeV.size(), path, 0, shortest);
+    solve(maze, mouse, cheese, path, 0, shortest);
 
     printf("\n");
-    for (int i = 0; i < path.size(); i ++) {
+    for (int i = 0; i < 12; i ++) {
         printf("%s\n", path[i].c_str());
     }
 }
