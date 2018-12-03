036a259df5d1f4e42ee2e6baed7588679c64c74e
S. M. Chase <smchase@protonmail.com>
2018-12-03 08:42:53 -0500

    removed cheese variable

--- Ass85.cpp
+++ Ass85.cpp
@@ -12,38 +12,38 @@ struct coord {
     vector<string> path;
 };
 
-void solve (vector<coord> &coordList, vector<string> &maze, array<int, 2> &cheese, int i) {
-    if (coordList[i].pos == cheese) {
+void solve (vector<coord> &coordList, vector<string> &maze, int i) {
+    if (maze[coordList[i].pos[0]][coordList[i].pos[1]] == 'C') {
         printf("\nShortest Path:\n");
         for (int j = 0; j < coordList[i].path.size(); j ++) {
             printf("%i. %s\n", j+1, coordList[i].path[j].c_str());
         }
     } else {
         if (maze[coordList[i].pos[0]-1][coordList[i].pos[1]] == ' ' || maze[coordList[i].pos[0]-1][coordList[i].pos[1]] == 'C') {
-            maze[coordList[i].pos[0]-1][coordList[i].pos[1]] = '+';
+            if (maze[coordList[i].pos[0]-1][coordList[i].pos[1]] != 'C') maze[coordList[i].pos[0]-1][coordList[i].pos[1]] = '+';
             coordList[i].path.push_back("UP");
             coordList.push_back({{coordList[i].pos[0]-1, coordList[i].pos[1]}, coordList[i].path});
             coordList[i].path.pop_back();
         }
         if (maze[coordList[i].pos[0]+1][coordList[i].pos[1]] == ' ' || maze[coordList[i].pos[0]+1][coordList[i].pos[1]] == 'C') {
-            maze[coordList[i].pos[0]+1][coordList[i].pos[1]] = '+';
+            if (maze[coordList[i].pos[0]+1][coordList[i].pos[1]] != 'C') maze[coordList[i].pos[0]+1][coordList[i].pos[1]] = '+';
             coordList[i].path.push_back("DOWN");
             coordList.push_back({{coordList[i].pos[0]+1, coordList[i].pos[1]}, coordList[i].path});
             coordList[i].path.pop_back();
         }
         if (maze[coordList[i].pos[0]][coordList[i].pos[1]-1] == ' ' || maze[coordList[i].pos[0]][coordList[i].pos[1]-1] == 'C') {
-            maze[coordList[i].pos[0]][coordList[i].pos[1]-1] = '+';
+            if (maze[coordList[i].pos[0]][coordList[i].pos[1]-1] != 'C') maze[coordList[i].pos[0]][coordList[i].pos[1]-1] = '+';
             coordList[i].path.push_back("LEFT");
             coordList.push_back({{coordList[i].pos[0], coordList[i].pos[1]-1}, coordList[i].path});
             coordList[i].path.pop_back();
         }
         if (maze[coordList[i].pos[0]][coordList[i].pos[1]+1] == ' ' || maze[coordList[i].pos[0]][coordList[i].pos[1]+1] == 'C') {
-            maze[coordList[i].pos[0]][coordList[i].pos[1]+1] = '+';
+            if (maze[coordList[i].pos[0]][coordList[i].pos[1]+1] != 'C') maze[coordList[i].pos[0]][coordList[i].pos[1]+1] = '+';
             coordList[i].path.push_back("RIGHT");
             coordList.push_back({{coordList[i].pos[0], coordList[i].pos[1]+1}, coordList[i].path});
             coordList[i].path.pop_back();
         }
-        solve(coordList, maze, cheese, i+1);
+        solve(coordList, maze, i+1);
     }
 }
 
@@ -58,11 +58,10 @@ int main ()
     vector<string> maze;
     while (getline(file, out)) maze.push_back(out);
 
-    array<int, 2> mouse, cheese;
+    array<int, 2> mouse;
     for (int i = 0; i < maze.size(); i ++) {
         for (int j = 0; j < maze[i].length(); j ++) {
             if (maze[i][j] == 'M') mouse = {i, j};
-            if (maze[i][j] == 'C') cheese = {i, j};
             printf("%c", maze[i][j]);
         }
         printf("\n");
@@ -70,5 +69,5 @@ int main ()
 
     vector<coord> coordList;
     coordList.push_back({mouse, {}});
-    solve(coordList, maze, cheese, 0);
+    solve(coordList, maze, 0);
 }
