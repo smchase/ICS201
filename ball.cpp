892676dc9947a46108dc053c49976b83a8a3feb9
S. M. Chase <smchase@protonmail.com>
2018-12-06 14:53:23 -0500

    gonna stop ball and start snake

--- ball.cpp
+++ ball.cpp
@@ -3,97 +3,83 @@
 #include <string>
 #include <stdio.h>
 #include <array>
+#include <math.h>
 #include <algorithm>
 
 using namespace std;
 
-void ClearScreen ()
+void bounce (array<string, 30> arr, int spd = 320)
 {
-  HANDLE                     hStdOut;
-  CONSOLE_SCREEN_BUFFER_INFO csbi;
-  DWORD                      count;
-  DWORD                      cellCount;
-  COORD                      homeCoords = { 0, 0 };
-
-  hStdOut = GetStdHandle( STD_OUTPUT_HANDLE );
-  if (hStdOut == INVALID_HANDLE_VALUE) return;
-
-  /* Get the number of cells in the current buffer */
-  if (!GetConsoleScreenBufferInfo( hStdOut, &csbi )) return;
-  cellCount = csbi.dwSize.X *csbi.dwSize.Y;
-
-  /* Fill the entire buffer with spaces */
-  if (!FillConsoleOutputCharacter(
-    hStdOut,
-    (TCHAR) ' ',
-    cellCount,
-    homeCoords,
-    &count
-    )) return;
-
-  /* Fill the entire buffer with the current colors and attributes */
-  if (!FillConsoleOutputAttribute(
-    hStdOut,
-    csbi.wAttributes,
-    cellCount,
-    homeCoords,
-    &count
-    )) return;
-
-  /* Move the cursor home */
-  SetConsoleCursorPosition( hStdOut, homeCoords );
-}
-
-void bounce (array<string, 25> arr, string dir = "down")
-{
-    bool first = true;
+    int first = -1;
+    printf("\n");
     for (int i = 0; i < arr.size(); i ++) {
-        if (find(arr[i].begin(), arr[i].end(), 'O') != arr[i].end() && first) {
-            if (dir == "down" && find(arr[i+3].begin(), arr[i+3].end(), '#') == arr[i+3].end()) {
-                arr[i].swap(arr[i+3]);
-            } else {
-                dir = (dir == "down" ? "up" : "down");
-            }
-            first = false;
-        } else if (dir == "up") {
-            if 
+        printf("%s", arr[i].c_str());
+        if (i < arr.size()-1) {
+            printf("\n");
         }
-        printf("%s\n", arr[i].c_str());
+        if (find(arr[i].begin(), arr[i].end(), 'O') != arr[i].end() && first == -1) {
+            first = i;
+        }
     }
-    Sleep(250);
-    ClearScreen();
-    bounce(arr, dir);
+
+    if (spd > 0) {
+        if (first == arr.size()-3) {
+            spd -= (2*spd);
+            swap(arr[first+2], arr[first-1]);
+            spd += first;
+        } else {
+            swap(arr[first], arr[first+3]);
+            spd -= first;
+        }
+    } else {
+        if (first == 0) {
+            spd += (2*abs(spd));
+            swap(arr[first], arr[first+3]);
+            spd -= first;
+        } else {
+            swap(arr[first+2], arr[first-1]);
+            spd += first;
+        }
+    }
+
+    Sleep(abs(spd));
+    bounce(arr, spd);
 }
 
 int main ()
 {
-    array<string, 25> arr =
+    array<string, 30> arr =
     {
-        "                                                  ",
-        "                       OOOO                       ",
-        "                       OOOO                       ",
-        "                       OOOO                       ",
-        "                                                  ",
-        "                                                  ",
-        "                                                  ",
-        "                                                  ",
-        "                                                  ",
-        "                                                  ",
-        "                                                  ",
-        "                                                  ",
-        "                                                  ",
-        "                                                  ",
-        "                                                  ",
-        "                                                  ",
-        "                                                  ",
-        "                                                  ",
-        "                                                  ",
-        "                                                  ",
-        "                                                  ",
-        "                                                  ",
-        "                                                  ",
-        "                                                  ",
-        "##################################################"
+        "                  OOOO                  ",
+        "                  OOOO                  ",
+        "                  OOOO                  ",
+        "                                        ",
+        "                                        ",
+        "                                        ",
+        "                                        ",
+        "                                        ",
+        "                                        ",
+        "                                        ",
+        "                                        ",
+        "                                        ",
+        "                                        ",
+        "                                        ",
+        "                                        ",
+        "                                        ",
+        "                                        ",
+        "                                        ",
+        "                                        ",
+        "                                        ",
+        "                                        ",
+        "                                        ",
+        "                                        ",
+        "                                        ",
+        "                                        ",
+        "                                        ",
+        "                                        ",
+        "                                        ",
+        "                                        ",
+        "                                        "
     };
     bounce(arr);
 }
