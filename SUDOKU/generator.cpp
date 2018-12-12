603667cd6a94ea89a873d05774e0203398b42b02
S. M. Chase <smchase@protonmail.com>
2018-12-12 15:04:54 -0500

    started sudoku generator

--- SUDOKU/generator.cpp
+++ SUDOKU/generator.cpp
@@ -8,5 +8,16 @@ using namespace std;
 
 int main ()
 {
-    
+    srand(time(NULL));
+    array<int, 9> nums;
+    array<array<int, 3>, 3> small;
+    array<array<array<array<int, 3>, 3>, 3>, 3> big;
+
+    nums = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    for (int i = 0; i < 3; i ++) {
+        for (int j = 0; j < 3; j ++) {
+            small[i][j] = nums[rand()%nums.size()];
+
+        }
+    }
 }
