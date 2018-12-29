9edf0df7c0ce196e8dd2a06839943b30539b46a2
Simon-Chase <smchase@protonmail.com>
2018-12-29 12:57:50 -0500

    i kinda get it

--- SUDOKU/solveProblem.cpp
+++ SUDOKU/solveProblem.cpp
@@ -6,13 +6,27 @@
 
 using namespace std;
 
-struct node
-{
-    node *header, *row, *left, *right, *up, *down;
-    int num, headerID, rowID;
+struct node {
+    node *header, *left, *right, *up, *down;
+    int num;
 };
 
-int main ()
-{
-    array<node, 9*9*9*5+9*9*4+1> matrix;
+int main () {
+    array<node, 10*9*9*4+1> matrix;
+
+    matrix[0].left = &matrix[0];
+    matrix[0].right = &matrix[0];
+
+    for (int i = 1; i < 9*9*4+1; i ++) {
+        matrix[i].up = &matrix[i];
+        matrix[i].down = &matrix[i];
+        matrix[i].left = &matrix[i-1];
+        matrix[i-1].right = &matrix[i];
+    }
+
+    for (int i = 9*9*4+1; i < matrix.size(); i ++) {
+        
+    }
+
+    cout << "Hello world!" << endl;
 }
