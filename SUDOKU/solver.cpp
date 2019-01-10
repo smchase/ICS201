4d4a0b113971c3ebbab9feb9cf0e3fe11f842485
S. M. Chase <smchase@protonmail.com>
2019-01-10 14:55:11 -0500

    finished filling body

--- SUDOKU/solver.cpp
+++ SUDOKU/solver.cpp
@@ -35,8 +35,8 @@ struct node2d {
 
 int main () {
     array<array<node2d, 9*9*4>, 9*9*9> matrix2d;
-    array<node, 9*9*9*4> matrix;
-    int it = 0;
+    array<node, (10*9*9*4)+1> matrix;
+    int it = (9*9*4)+1;
 
     for (int i = 0; i < 9*9*9; i ++) {
         matrix2d[i][i/9] = {true, &matrix[it]};
@@ -59,8 +59,79 @@ int main () {
         matrix[it].pos = {i/81, (i/9)%9};
         it ++;
     }
+    cout << "created 2d matrix" << endl;
 
-    cout << "created 2d matrix!" << endl;
+    matrix[0].left = &matrix[9*9*4];
+    for (int i = 1; i < (9*9*4)+1; i ++) {
+        matrix[i].up = &matrix[i];
+        matrix[i].down = &matrix[i];
+        matrix[i].left = &matrix[i-1];
+        matrix[i-1].right = &matrix[i];
+    }
+    matrix[9*9*4].right = &matrix[0];
+    cout << "created headers" << endl;
+
+    node *leftNode;
+    for (int i = 0; i < 9*9*9; i ++) {
+        for (int j = 0; j < 9*9*4; j ++) {
+            if (matrix2d[i][j].filled) {
+                // header
+                matrix2d[i][j].pointer->header = &matrix[j+1];
+
+                // up
+                for (int n = i; n >= 0; n --) {
+                    if (matrix2d[n][j].filled) {
+                        matrix2d[i][j].pointer->up = matrix2d[n][j].pointer;
+                        break;
+                    } else if (n == 0) {
+                        matrix2d[i][j].pointer->up = &matrix[j+1];
+                        matrix[j+1].down = matrix2d[i][j].pointer;
+                    }
+                }
+
+                // down
+                for (int n = i; n < 9*9*9; n ++) {
+                    if (matrix2d[n][j].filled) {
+                        matrix2d[i][j].pointer->down = matrix2d[n][j].pointer;
+                        break;
+                    } else if (n+1 == 9*9*9) {
+                        matrix2d[i][j].pointer->down = &matrix[j+1];
+                        matrix[j+1].up = matrix2d[i][j].pointer;
+                    }
+                }
+
+                // left
+                node *leftNode;
+                for (int n = j; n >= 0; n --) {
+                    if (matrix2d[i][n].filled) {
+                        matrix2d[i][j].pointer->left = matrix2d[i][n].pointer;
+                        break;
+                    } else if (n == 0) {
+                        leftNode = matrix2d[i][j].pointer;
+                    }
+                }
+
+                // right
+                for (int n = j; n < 9*9*4; n ++) {
+                    if (matrix2d[i][n].filled) {
+                        matrix2d[i][j].pointer->right = matrix2d[i][n].pointer;
+                        break;
+                    } else if (n+1 == 9*9*4) {
+                        matrix2d[i][j].pointer->right = leftNode;
+                        leftNode->left = matrix2d[i][j].pointer;
+                    }
+                }
+            }
+        }
+    }
+    cout << "filled body pointers" << endl;
+
+    /*
+    node *n = &matrix[0];
+    do {
+        n = n->right;
+    } while (n != &matrix[0]);
+    */
 
     cout << "finished!" << endl;
 }
